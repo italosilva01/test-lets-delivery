ad88dd0453a2afbd353fe91e4243b78b0d093387
Francisco Italo Silva do Nascimento <italo.silvan87@gmail.com>
2021-06-21 18:30:44 -0300

    adicionar configuração redux toolkit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
 import { Header } from "components/Header";
 import { CharacterList } from "components/CharacterList";
-
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "store";
+import { insert } from "store/Stock.store";
+import { Character } from "model/character";
 export const Home = () => {
+  const stock = useSelector((state: RootState) => state.stock);
+  const dispatch = useDispatch();
   return (
     <>
       <Header />
+
       <CharacterList />
+      {/*  {stock.[0]} */}
+      {/* <button onClick={() => dispatch(insert({} as Character))}>
+        increment
+      </button> */}
     </>
   );
 };
